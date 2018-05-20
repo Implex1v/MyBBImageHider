3cd53e17df2bac68f5edbd11a73b9433a4fd13a9
Implex1v <implex1v@gmail.com>
2018-05-20 14:35:09 +0200

    Add first version

---
+++ inc/plugins/image_hider.php
@@ -0,0 +1,281 @@
+<?php
+/* Author: Implex1v <Implex1v@gmail.com>
+ * Licence: Attribution-ShareAlike 4.0 International (CC BY-SA 4.0): https://creativecommons.org/licenses/by-sa/4.0/ */
+
+$plugins->add_hook("pre_output_page", ["ImageHider", "clear_images"]);
+
+function image_hider_info() {
+    return [
+        'name'           => 'Image Hider',
+        'description'    => 'Hides images whose URL is not whitelisted',
+        'website'        => 'https://implex1v.de/',
+        'author'         => 'Implex1v',
+        'authorsite'     => 'https://implex1v.de/',
+        'version'        => '1.0',
+        'codename'       => 'image_hider',
+        'compatibility'  => '18*',
+    ];
+}
+
+function image_hider_install() {
+    global $db;
+
+    $settingGroupId = $db->insert_query('settinggroups', [
+      'name'        => 'image_hider',
+      'title'       => 'Image Hider',
+      'description' => 'Hides images whose URL is not whitelisted.',
+    ]);
+
+    $settings =
+        [[
+            'name'        => 'image_hider_activated',
+            'title'       => 'Activated',
+            'description' => 'Indicates if hiding images is active or not.',
+            'optionscode' => 'onoff',
+            'value'       => '1'
+        ],[
+            'name'        => 'image_hider_whitelist',
+            'title'       => 'Semicolon sperated list of whitelisted URLs',
+            'description' => 'A list of semicolon seperated URLs which are whitelisted for displaying the image. Please do not use any whitepasces. Remember to whitelist your own website.',
+            'optionscode' => 'text',
+            'value'       => 'example.com;'
+        ],[
+            'name'        => 'image_hider_replacement',
+            'title'       => 'The text the matched images will be replaced with',
+            'description' => 'Defines what the found images will be replaced with. You can use HTML but short code is recommended. Use <tt>{src}</tt> for the original URL to the image.',
+            'optionscode' => 'text',
+            'value'       => '<span>[Image: <a href="{src}">{src}</a>]</span>'
+        ],[
+            'name'        => 'image_hider_replacement_image',
+            'title'       => 'The replacement image which will be displayed if linking is not possible.',
+            'description' => 'Defines a URL to the replacement image if the linking of the image is not possible e.g. in CSS. Be sure to whitelist the url of the replacement image.',
+            'optionscode' => 'text',
+            'value'       => 'images/404.gif'
+        ]];
+
+    $i = 1;
+    foreach ($settings as &$row) {
+        $row['gid']       = $settingGroupId;
+        $row['disporder'] = $i++;
+        $db->insert_query("settings", $row);
+    }
+
+    rebuild_settings();
+}
+
+function image_hider_uninstall() {
+    global $db;
+
+    $settingGroupId = $db->fetch_field(
+        $db->simple_select('settinggroups', 'gid', "name='image_hider'"),
+        'gid'
+    );
+
+    $db->delete_query('settinggroups', 'gid=' . $settingGroupId);
+     rebuild_settings();
+}
+
+function image_hider_is_installed() {
+    global $db;
+
+    // manual check to avoid caching issues
+    $query = $db->simple_select('settinggroups', 'gid', "name='image_hider'");
+    return (bool)$db->num_rows($query);
+}
+
+class ImageHider {
+    /**
+     * Called by 'pre_output_page' hook. Removes all not whitelisted images from a website. The setting 'image_hider_activated' is used
+     * to check if the plugin is activated. The setting 'image_hider_whitelist' is used to get all whitelisted image hosts.
+     * @param $page string The whole page to output
+     */
+    static function clear_images(&$page) {
+        global $db;
+
+        $activated = $db->query("SELECT value FROM mybb_settings WHERE name = 'image_hider_activated'")->fetch_array()['value'];
+        if($activated) {
+            $urlDB = $db->query("SELECT value FROM mybb_settings WHERE name = 'image_hider_whitelist'")->fetch_array()['value'];
+            $urls = explode(";", $urlDB);
+            if($urls[sizeof($urls)-1] == "") {
+                unset($urls[sizeof($urls)-1]);
+            }
+
+            $cache = array();
+            ImageHider::clear_img($page, $urls, $cache);
+            ImageHider::clear_bb($page, $urls, $cache);
+            ImageHider::clear_others($page, $urls, $cache);
+        }
+    }
+
+    /**
+     * Replaces all not whitelisted images in HTML-Code. Found images will be replaced with the value of the 'image_hider_replacement' setting.
+     * @param $page string The whole page to output
+     * @param $urls array The list of whitelisted URLs
+     * @param $cache array A cache of already replaced links. Used to add found URLs
+     */
+    static function clear_img(&$page, &$urls, &$cache) {
+        global $db;
+
+        $preg = "/(<img\s)[a-zA-Z0-9:;-=\"'\s\.\{\[\]\}\&\?\$\/\_\\\\]+(\/)?(>)/im";
+        preg_match_all($preg, $page, $matches, PREG_OFFSET_CAPTURE);
+        foreach ($matches[0] as $value) {
+            $src = ImageHider::extract_url($value[0]);
+
+            if($src === FALSE) {
+                $src = ImageHider::extract_url_idiots($value[0]);
+                if($src === FALSE) {
+                    continue;
+                }
+            }
+
+            $matched = false;
+            foreach($urls as $url) {
+                if(strpos($src, $url) !== FALSE) {
+                    $matched = true;
+                }
+            }
+
+            // check if local images are included
+            if(ImageHider::starts_with($src, "images/")) {
+                $matched = true;
+            }
+
+            if(!$matched) {
+                $cache[] = $src;
+
+                $replacement = $db->query("SELECT value FROM mybb_settings WHERE name = 'image_hider_replacement'")->fetch_array()['value'];
+                $replaced = str_replace("{src}", $src, $replacement);
+                $page = str_replace($value, $replaced, $page);
+            }
+        }
+    }
+
+    /**
+     * Replaces all not whitelisted images in BB-Code. Found images will be replaced with the value of the 'image_hider_replacement' setting.
+     * @param $page string The whole page to output
+     * @param $urls array The list of whitelisted URLs
+     * @param $cache array A cache of already replaced links. Used to add found URLs
+     */
+    static function clear_bb(&$page, &$urls, &$cache) {
+        global $db;
+
+        $reg = "/(\[\s*img\s*\])([a-zA-Z0-9\.\/\:\?\&\s\=\-\_\;\%]+)(\[\s*\/\s*img\s*\])/im";
+        preg_match_all($reg, $page, $matches);
+
+        foreach($matches[2] as $key => $value) {
+            $matched = false;
+            foreach($urls as $url) {
+                if(strpos($value, $url) !== FALSE) {
+                    $matched = true;
+                }
+            }
+
+            // check if local images are included
+            if(ImageHider::starts_with($value, "images/")) {
+                $matched = true;
+            }
+
+            if(!$matched) {
+                $cache[] = $value;
+
+                $replacement = $db->query("SELECT value FROM mybb_settings WHERE name = 'image_hider_replacement'")->fetch_array()['value'];
+                $replaced = str_replace("{src}", $matched[0][$key], $replacement);
+                $page = str_replace($value, $replaced, $page);
+            }
+        }
+    }
+
+    /**
+     * Replaces all other found images with a defined replacement image. The replacement image can be set in the ACP settings (key=image_hider_replacement_image).
+     * @param $page string The whole page to output
+     * @param $urls array The list of whitelisted URLs
+     * @param $cache array A cache of already replaced links. Used to check if url is already replaced
+     */
+    static function clear_others(&$page, &$urls, &$cache) {
+        global $db;
+
+        $reg = "/[a-zA-Z0-9\.\/\?\&\:\;\_\-]*\/[a-zA-Z0-9\.\/\?\&\:\;\_\-]*(\.gif|\.jpg|\.png|\.jpeg|\.bmp|\.svg|\.tif)/im";
+        preg_match_all($reg, $page, $matches);
+
+        foreach($matches[0] as $value) {
+            $src = $value;
+            $matched = false;
+            foreach($urls as $url) {
+                if(strpos($src, $url) !== FALSE) {
+                    $matched = true;
+                }
+            }
+
+            // check if local images are included
+            if(ImageHider::starts_with($src, "images/")) {
+                $matched = true;
+            }
+
+            if(!$matched && !in_array($src, $cache)) {
+                $replacement = $db->query("SELECT value FROM mybb_settings WHERE name = 'image_hider_replacement_image'")->fetch_array()['value'];
+                $page = str_replace($value, $replacement, $page);
+            }
+        }
+    }
+
+    /**
+     * Extracts a url from a src tag when the author did not use quotes ....
+     * @param $img_tag string The whole img-tag
+     * @return bool|string The link if matched otherwise <code>FALSE</code>
+     */
+    static function extract_url_idiots($img_tag) {
+        $preg = "/(src)(\s)*=(\s)*[a-zA-Z0-9\?\&\#:\/\\\\;:\s\-\_\.\%]*(>)/im";
+        preg_match($preg, $img_tag, $match);
+
+        if(sizeof($match) > 0) {
+            $src = $match[0];
+            $equal = strpos($src, "=");
+
+            $linkBegin = $equal;
+            for(; $linkBegin < sizeof($src); $linkBegin++) {
+                if(! preg_match("/\s/", substr($src, $equal, 1))) {
+                    break;
+                }
+            }
+
+            return substr($src, $linkBegin+1, sizeof($src)-2);
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+     * Extracts a url from a src tag
+     * @param $img_tag string The whole img-tag
+     * @return bool|string The link if matched otherwise <code>FALSE</code>
+     */
+    static function extract_url($img_tag) {
+        $preg = "/(src)(\s)*=(\s)*(\"|\')[a-zA-Z0-9\?\&\#:\/\\\\;:\s\-\_\.\%]*(\"|\')/im";
+        preg_match($preg, $img_tag, $match);
+
+        if(sizeof($match) > 0) {
+            $src = $match[0];
+            $lastQuote = substr($src, -1);
+            $firstQuote = strpos($src, $lastQuote);
+
+            if($firstQuote) {
+                return substr($src, $firstQuote+1, sizeof($src)-2);
+            } else {
+                return FALSE;
+            }
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+     * Checks if <code>$haystack</code> starts with <code>$needle</code>
+     * @param $haystack
+     * @param $needle
+     * @return bool <code>TRUE</code> if <code>$haystack</code> starts with <code>$needle</code> otherwise <code>FALSE</code>
+     */
+    static function starts_with($haystack, $needle) {
+         $length = strlen($needle);
+         return (substr($haystack, 0, $length) === $needle);
+    }
+}
